a03eca771ba5d7acb645cdf6d1cb2e5fd91e1498
Boo <boojh811@gmail.com>
2023-05-06 19:31:33 +0900

    feat : modify models.py

--- session-02/김승환-부준혁/Django-Blog-Template-main/blogapp/models.py
+++ session-02/김승환-부준혁/Django-Blog-Template-main/blogapp/models.py
@@ -8,3 +8,17 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+# 커뮤니티 게시판 
+
+class Post(models.Model):
+    # image = models.ImageField(verbose_name='이미지') # 게시글 이미지 저장 필드  -- 이미지는 일단 보류
+    
+    content = models.TextField('내용') # 게시글 내용 저장 필드 
+    created_at = models.DateTimeField('작성일') # 작성 시간
+    view_count = models.IntegerField('조회수')  # 조회수   
+
+# 댓글 모델
+
+#class Comment(models.Model)
