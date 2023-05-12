434a0f337be911eab60d5654f54bbdc12801311b
kseysh <kseysh123@naver.com>
2023-05-12 14:41:39 +0900

    feat : 회원가입 오류 메시지 수정

--- session-02/김승환-부준혁/Django-Blog-Template-main/accounts/views.py
+++ session-02/김승환-부준혁/Django-Blog-Template-main/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login,logout,authenticate
 from .forms import UserCreateForm, SignUpForm, LoginForm
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib import messages
 
 def signup(request):
